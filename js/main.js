d09877d1a69d0c60bcf1b9856e1ac2ee44c6e754
Axel JARNIGON <axel.jarnigon@gmail.com>
2021-02-25 23:18:37 +0100

    compare choice and show result

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ var userName = prompt("Quel est ton nom ?");
 alert("Attention " + userName + " es tu sûr de vouloir te mesurer à moi ?");
 
 //Getting Player's choice and CPU's choice
-var userChoice = prompt("Fais ton choix :\nPierre, Feuille ou Ciseaux ?" );
+var userChoice = prompt("Fais ton choix :\nPierre, Feuille ou Ciseaux ?" ).toLowerCase();
 
 var cpuChoice = Math.floor(Math.random()*3);
 
@@ -22,3 +22,16 @@ if (cpuChoice === 2){
 else {
     cpuChoice = "ciseaux";
 };
+
+alert("Hum, je choisis : " + cpuChoice);
+
+//Comparing choices and giving the results
+if (cpuChoice === userChoice){
+    alert("Egalité !\nPas de perdants,ni de gagnants.");
+}
+else if ((cpuChoice === "feuille" && userChoice === "ciseaux") || (cpuChoice === "pierre" && userChoice === "feuille") || (cpuChoice === "ciseaux" && userChoice === "pierre")){
+    alert("Bien joué, tu as gagné " + userName);
+}
+else {
+    alert("T'as perdu. T'es qu'une merde " + userName + " !");
+}
