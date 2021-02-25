a858b5e6abc5e0dbb8cc89a29791fb7775b85541
Axel Jarnigon <axel.jarnigon@gmail.com>
2021-02-25 15:50:51 +0100

    Added Welcome message and username prompt

---
+++ js/main.js
@@ -0,0 +1,5 @@
+//Affichage du message de bienvenue
+alert("Bienvenue dans l'antre du Chifoumi, voulez vous jouer avec moi?");
+
+//Demande le nom du joueur (userName)
+var userName = prompt("Quel est ton nom ?");
