85b58bba851ee3d171f855c8e224f16401fafc25
Axel JARNIGON <axel.jarnigon@gmail.com>
2021-02-25 22:32:08 +0100

    transform cpu choice into a string variable

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
 //Showing a welcome message
 alert("Bienvenue dans l'antre du Chifoumi, voulez vous jouer avec moi?");
 
-//Asking for the name of the player (userName)
+//Asking for player's name (userName)
 var userName = prompt("Quel est ton nom ?");
 
 //Taunting the player
 alert("Attention " + userName + " es tu sûr de vouloir te mesurer à moi ?");
 
+//Getting Player's choice and CPU's choice
 var userChoice = prompt("Fais ton choix :\nPierre, Feuille ou Ciseaux ?" );
+
+var cpuChoice = Math.floor(Math.random()*3);
+
+//Turning CPU's choice into a string value
+if (cpuChoice === 2){
+    cpuChoice = "pierre";
+}
+    else if (cpuChoice === 1){
+    cpuChoice = "feuille";
+}
+else {
+    cpuChoice = "ciseaux";
+};
