13992688c465dcb7044bad6ceb390ae6cb13a3c7
Axel JARNIGON <axel.jarnigon@gmail.com>
2021-02-25 22:05:45 +0100

    added var for player and cpu choice

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-//Affichage du message de bienvenue
+//Showing a welcome message
 alert("Bienvenue dans l'antre du Chifoumi, voulez vous jouer avec moi?");
 
-//Demande le nom du joueur (userName)
+//Asking for the name of the player (userName)
 var userName = prompt("Quel est ton nom ?");
+
+//Taunting the player
+alert("Attention " + userName + " es tu sûr de vouloir te mesurer à moi ?");
+
+var userChoice = prompt("Fais ton choix :\nPierre, Feuille ou Ciseaux ?" );
